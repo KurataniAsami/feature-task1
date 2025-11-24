72412c150068b9f89fe35e25050001ee5a4156c5
kurataniasami <kura56135613@gmail.com>
2025-11-24 11:11:32 +0900

    index.jsの中身を変更

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Home from './Home';
-import Posts from './data/Posts';
 import { posts } from './data/Posts'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <Home posts={posts}/>
+    <App posts={posts}/>
   </React.StrictMode>
 );
 
