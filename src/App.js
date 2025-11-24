5f3a76d286041280b68f7b8e70cbac9ab933d6fd
kurataniasami <kura56135613@gmail.com>
2025-11-24 11:09:56 +0900

    Home.jsの中身をApp.jsへ移動

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 
-const App = () => {
+export default function App({ posts }) {
   return (
-    <div>課題の1</div>
-  )
+    <div className="App">
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <div className='flex-row'>
+              <div>{post.createdAt}</div>
+              <div className='category-tag'>{post.categories}</div>
+            </div>
+
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
 
